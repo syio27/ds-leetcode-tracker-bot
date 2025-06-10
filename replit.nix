755fb56f6c60a6cde4a1209727476c97a2fa473b
Baglan <baglan@mac.home>
2025-06-11 01:47:03 +0200

    Remove Replit-specific files for Oracle Cloud migration

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{pkgs}: {
-  description = "Java Discord Bot";
-  deps = [
-    pkgs.graalvm17-ce
-    pkgs.gradle
-    pkgs.replitPackages.jdt-language-server
-    pkgs.replitPackages.java-debug
-  ];
-} 
